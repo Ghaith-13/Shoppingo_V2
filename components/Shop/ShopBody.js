49b16ce1c5bab7589d5322aad23f67b3a380f7b5
Ghaith-13 <ghaethoo2001.go@gmail.com>
2023-09-12 05:45:11 -0400

    fix stuff

--- components/Shop/ShopBody.js
+++ components/Shop/ShopBody.js
@@ -1,129 +1,215 @@
-import React, { useEffect, useState } from "react";
-import { TiArrowSortedDown } from "react-icons/ti";
-import Classification from "./Classification";
-import PriceClassification from "./PriceClassification";
-import Search from "./Search";
-import Placeholder from "./Placeholder";
+import React, { useEffect, useState } from 'react';
+import { TiArrowSortedDown } from 'react-icons/ti';
+import Classification from './Classification';
+import PriceClassification from './PriceClassification';
+import Search from './Search';
+import Placeholder from './Placeholder';
 import dynamic from 'next/dynamic';
-const DynamicProduct=dynamic(()=>import('./Product'),{
-  loading: () => <Placeholder/> ,
+const DynamicProduct = dynamic(() => import('./Product'), {
+  loading: () => <Placeholder />,
   ssr: false,
-})
+});
 import ReactPaginateComponent from '../../components/ReactPaginateComponent';
-import Lottie from "lottie-react";
-import emptyResult from "../../public/empty.json";
+import Lottie from 'lottie-react';
+import emptyResult from '../../public/empty.json';
 
-import { useTheme } from "next-themes";
-import { parseCookies } from "nookies";
-import { useRouter } from "next/router";
+import { useTheme } from 'next-themes';
+import { parseCookies } from 'nookies';
+import { useRouter } from 'next/router';
 
-
-const ShopBody = ({ shopRoute, offersRoute, shopIdRoute , setNoteMsg ,uniqueProducts }) => {
-
+const ShopBody = ({
+  shopRoute,
+  offersRoute,
+  shopIdRoute,
+  setNoteMsg,
+  uniqueProducts,
+}) => {
   const cookies = parseCookies();
   const role = cookies.role;
-  const router =useRouter();
+  const router = useRouter();
 
   const { theme, setTheme } = useTheme();
-  const [logoUrl,setLogoUrl]=useState('');
-  useEffect(()=>{
-    if(theme == 'light'){
-      setLogoUrl('../logo.svg')
-    }else{
-      setLogoUrl('../darkLogo.svg')
+  const [logoUrl, setLogoUrl] = useState('');
+  useEffect(() => {
+    if (theme == 'light') {
+      setLogoUrl('../logo.svg');
+    } else {
+      setLogoUrl('../darkLogo.svg');
     }
-  },[theme])
+  }, [theme]);
 
-  const [PShow,setPShow]=useState(false);
-  const [MShow,setMShow]=useState(false);
-  const [WShow,setWShow]=useState(false);
-  const [selectedOptionsForFemale,setSelectedOptionsForFemale]=useState([]);
-  const [selectedOptionsForMale,setSelectedOptionsForMale]=useState([]);
+  const [PShow, setPShow] = useState(false);
+  const [MShow, setMShow] = useState(false);
+  const [WShow, setWShow] = useState(false);
+  const [selectedOptionsForFemale, setSelectedOptionsForFemale] = useState([]);
+  const [selectedOptionsForMale, setSelectedOptionsForMale] = useState([]);
 
-  const [products,setProducts]=useState(uniqueProducts);
+  const [products, setProducts] = useState(uniqueProducts);
 
-  const [productsPerPage,setProductsPerPage]=useState(5);
+  const [productsPerPage, setProductsPerPage] = useState(5);
   const [currentPage, setCurrentPage] = useState(0); // Current page state
-  const [productsDisplayed, setProductsDisplayed] = useState( products?.slice(0,productsPerPage) );
+  const [productsDisplayed, setProductsDisplayed] = useState(
+    products?.slice(0, productsPerPage)
+  );
   const [FirstArrow, setFirstArrow] = useState(false);
-  const [LastArrow, setLastArrow] = useState(products?.length > productsPerPage);
+  const [LastArrow, setLastArrow] = useState(
+    products?.length > productsPerPage
+  );
 
   //* 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝘁𝗵𝗲 𝗳𝗶𝗿𝘀𝘁𝗔𝗿𝗿𝗼𝘄 , 𝗹𝗮𝘀𝘁𝗔𝗿𝗿𝗼𝘄 , 𝗰𝘂𝗿𝗿𝗲𝗻𝘁𝗣𝗮𝗴𝗲 𝗮𝗻𝗱 𝘁𝗵𝗲 𝗽𝗿𝗼𝗱𝘂𝗰𝘁𝘀𝗗𝗶𝘀𝗽𝗹𝗮𝘆𝗲𝗱 𝗯𝗮𝘀𝗲𝗱 𝗼𝗻 𝘁𝗵𝗲 𝗻𝗲𝘄 𝘀𝗲𝗹𝗲𝗰𝘁𝗲𝗱 𝗱𝗮𝘁𝗮
   const handleChange = (data) => {
-
     // 𝗳𝗼𝗿 𝗹𝗲𝗳𝘁 𝗮𝗿𝗿𝗼𝘄
-    if ( data.selected == 0 ) setFirstArrow(false);
+    if (data.selected == 0) setFirstArrow(false);
     else setFirstArrow(true);
 
     // 𝗳𝗼𝗿 𝗿𝗶𝗴𝗵𝘁 𝗮𝗿𝗿𝗼𝘄
-    if ( data.selected == ( Math.ceil(products.length / productsPerPage) - 1 ) ) setLastArrow(false);
+    if (data.selected == Math.ceil(products?.length / productsPerPage) - 1)
+      setLastArrow(false);
     else setLastArrow(true);
 
-    setCurrentPage(data.selected)
+    setCurrentPage(data.selected);
 
-    setProductsDisplayed(products.slice(data.selected * productsPerPage, data.selected * productsPerPage + productsPerPage));
-
+    setProductsDisplayed(
+      products.slice(
+        data.selected * productsPerPage,
+        data.selected * productsPerPage + productsPerPage
+      )
+    );
   };
 
-  useEffect(()=>{
-
+  useEffect(() => {
     setCurrentPage(0);
     setFirstArrow(false);
-    setLastArrow( products?.length > productsPerPage )
-    setProductsDisplayed( products?.slice(0,productsPerPage) );
+    setLastArrow(products?.length > productsPerPage);
+    setProductsDisplayed(products?.slice(0, productsPerPage));
+  }, [products]);
 
-  },[products])
-  
   return (
     <div
       className={
         shopIdRoute
           ? `flex flex-col`
-          : `w-full ${products.length == 0 ? 'min-h-screen md:h-screen' : 'min-h-screen' } pt-[103px] px-4 md:px-8 pb-7 flex flex-col`
+          : `w-full ${
+              products?.length == 0
+                ? 'min-h-screen md:h-screen'
+                : 'min-h-screen'
+            } pt-[103px] px-4 md:px-8 pb-7 flex flex-col`
       }
     >
       {/* Search and classification */}
       <div className="flex flex-col-reverse md:items-center md:flex-row md:justify-between">
         {/* Search */}
-        <Search setProducts={setProducts} uniqueProducts={uniqueProducts} shopRoute={shopRoute} setSelectedOptionsForFemale={setSelectedOptionsForFemale} setSelectedOptionsForMale={setSelectedOptionsForMale}/>
+        <Search
+          setProducts={setProducts}
+          uniqueProducts={uniqueProducts}
+          shopRoute={shopRoute}
+          setSelectedOptionsForFemale={setSelectedOptionsForFemale}
+          setSelectedOptionsForMale={setSelectedOptionsForMale}
+        />
         {/* Classification */}
         <div className="flex flex-col-reverse md:flex-row md:space-x-3 md:items-center">
           {/* woman */}
           <div className="relative z-10 flex flex-col group">
-            <button className={`flex justify-between items-center py-2 px-3 ${WShow ? 'bg-[#bb0202] dark:bg-gradient-to-tl' : '' } mt-5 md:mt-0`} onClick={()=> { setWShow(prev=>!prev);setMShow(false);setPShow(false); }}>
+            <button
+              className={`flex justify-between items-center py-2 px-3 ${
+                WShow ? 'bg-[#bb0202] dark:bg-gradient-to-tl' : ''
+              } mt-5 md:mt-0`}
+              onClick={() => {
+                setWShow((prev) => !prev);
+                setMShow(false);
+                setPShow(false);
+              }}
+            >
               <TiArrowSortedDown className="mr-1 w-5 h-5" /> نسائي
             </button>
 
-            <TiArrowSortedDown className={` absolute top-[50px] left-[8px] md:top-[30px] md:left-[30px] text-[#bb0202] dark:text-[#474747] dark:md:text-[#323232] w-7 h-7 ${ WShow ? 'flex' : 'hidden' } `} />
+            <TiArrowSortedDown
+              className={` absolute top-[50px] left-[8px] md:top-[30px] md:left-[30px] text-[#bb0202] dark:text-[#474747] dark:md:text-[#323232] w-7 h-7 ${
+                WShow ? 'flex' : 'hidden'
+              } `}
+            />
 
-            <Classification woman={true} WShow={WShow} setProducts={setProducts} uniqueProducts={uniqueProducts} shopRoute={shopRoute} selectedOptionsForMale={selectedOptionsForMale} selectedOptionsForFemale={selectedOptionsForFemale} setSelectedOptionsForFemale={setSelectedOptionsForFemale} setSelectedOptionsForMale={setSelectedOptionsForMale}/>
+            <Classification
+              woman={true}
+              WShow={WShow}
+              setProducts={setProducts}
+              uniqueProducts={uniqueProducts}
+              shopRoute={shopRoute}
+              selectedOptionsForMale={selectedOptionsForMale}
+              selectedOptionsForFemale={selectedOptionsForFemale}
+              setSelectedOptionsForFemale={setSelectedOptionsForFemale}
+              setSelectedOptionsForMale={setSelectedOptionsForMale}
+            />
           </div>
 
           {/* man */}
           <div className="relative z-10 flex flex-col group">
-            <button className={`flex justify-between items-center py-2 px-3 ${MShow ? 'bg-[#bb0202] dark:bg-gradient-to-tl' : '' } mt-5 md:mt-0`}
-            onClick={()=> { setMShow(prev=>!prev);setWShow(false);setPShow(false); }}>
+            <button
+              className={`flex justify-between items-center py-2 px-3 ${
+                MShow ? 'bg-[#bb0202] dark:bg-gradient-to-tl' : ''
+              } mt-5 md:mt-0`}
+              onClick={() => {
+                setMShow((prev) => !prev);
+                setWShow(false);
+                setPShow(false);
+              }}
+            >
               <TiArrowSortedDown className="mr-1 w-5 h-5" />
               رجالي
             </button>
 
-            <TiArrowSortedDown className={` absolute  top-[50px] left-[8px] md:top-[30px] md:left-[30px] text-[#bb0202] dark:text-[#474747] dark:md:text-[#323232] w-7 h-7 ${ MShow ? 'flex' : 'hidden' } `} />
+            <TiArrowSortedDown
+              className={` absolute  top-[50px] left-[8px] md:top-[30px] md:left-[30px] text-[#bb0202] dark:text-[#474747] dark:md:text-[#323232] w-7 h-7 ${
+                MShow ? 'flex' : 'hidden'
+              } `}
+            />
 
-            <Classification woman={false} MShow={MShow} setProducts={setProducts} uniqueProducts={uniqueProducts} shopRoute={shopRoute}
-            selectedOptionsForMale={selectedOptionsForMale} selectedOptionsForFemale={selectedOptionsForFemale} setSelectedOptionsForFemale={setSelectedOptionsForFemale} setSelectedOptionsForMale={setSelectedOptionsForMale}/>
+            <Classification
+              woman={false}
+              MShow={MShow}
+              setProducts={setProducts}
+              uniqueProducts={uniqueProducts}
+              shopRoute={shopRoute}
+              selectedOptionsForMale={selectedOptionsForMale}
+              selectedOptionsForFemale={selectedOptionsForFemale}
+              setSelectedOptionsForFemale={setSelectedOptionsForFemale}
+              setSelectedOptionsForMale={setSelectedOptionsForMale}
+            />
           </div>
 
           {/* price */}
           <div className="relative z-10 flex flex-col group">
-            <button className={`flex justify-between items-center py-2 px-3 ${PShow ? 'bg-[#bb0202] dark:bg-gradient-to-tl' : '' }  mt-5 md:mt-0`} onClick={() => { setPShow(prev=>!prev);setWShow(false);setMShow(false); }}>
+            <button
+              className={`flex justify-between items-center py-2 px-3 ${
+                PShow ? 'bg-[#bb0202] dark:bg-gradient-to-tl' : ''
+              }  mt-5 md:mt-0`}
+              onClick={() => {
+                setPShow((prev) => !prev);
+                setWShow(false);
+                setMShow(false);
+              }}
+            >
               <TiArrowSortedDown className="mr-1 w-5 h-5" />
               السعر
             </button>
 
-            <TiArrowSortedDown className={`absolute top-[50px] left-[8px] md:top-[30px] md:left-[30px] text-[#bb0202] dark:text-[#474747] dark:md:text-[#323232] w-7 h-7 ${PShow ? 'flex' : 'hidden' } `} />
+            <TiArrowSortedDown
+              className={`absolute top-[50px] left-[8px] md:top-[30px] md:left-[30px] text-[#bb0202] dark:text-[#474747] dark:md:text-[#323232] w-7 h-7 ${
+                PShow ? 'flex' : 'hidden'
+              } `}
+            />
 
             {/* drop down */}
-            <PriceClassification PShow={PShow} setPShow={setPShow} setProducts={setProducts} uniqueProducts={uniqueProducts} shopRoute={shopRoute} setNoteMsg={setNoteMsg} setSelectedOptionsForFemale={setSelectedOptionsForFemale} setSelectedOptionsForMale={setSelectedOptionsForMale}/>
+            <PriceClassification
+              PShow={PShow}
+              setPShow={setPShow}
+              setProducts={setProducts}
+              uniqueProducts={uniqueProducts}
+              shopRoute={shopRoute}
+              setNoteMsg={setNoteMsg}
+              setSelectedOptionsForFemale={setSelectedOptionsForFemale}
+              setSelectedOptionsForMale={setSelectedOptionsForMale}
+            />
           </div>
 
           <div className="self-center">: ترتيب حسب </div>
@@ -137,53 +223,77 @@ const ShopBody = ({ shopRoute, offersRoute, shopIdRoute , setNoteMsg ,uniqueProd
         <div className="w-1/2 h-[1px] bg-effectColor dark:bg-darkTextColor" />
       </div>
 
-      {
-        (role == 'seller' && !shopIdRoute ) && (
-          <div className='mb-7 flex space-x-3 justify-between items-center'>
+      {role == 'seller' && !shopIdRoute && (
+        <div className="mb-7 flex space-x-3 justify-between items-center">
+          <button
+            className="w-[100px] py-1"
+            onClick={() => router.push('/profile/sellerDashboard/order')}
+          >
+            طلباتي
+          </button>
 
-            <button className='w-[100px] py-1' onClick={() => router.push("/profile/sellerDashboard/order")}>
-              طلباتي
-            </button>
+          <button
+            className="w-[100px] py-1"
+            onClick={() => router.push('/profile/sellerDashboard/addProduct')}
+          >
+            إضافة منتج
+          </button>
+        </div>
+      )}
 
-            <button className='w-[100px] py-1' onClick={() => router.push("/profile/sellerDashboard/addProduct")}>
-              إضافة منتج
-            </button>
-
-          </div>
-        )
-      }
-      
-      {
-        products.length !== 0 ? (
-            <div dir="rtl" className="flex justify-evenly flex-wrap">
-
-            {
-                productsDisplayed.map((one,index)=>{
-                  if(shopRoute){
-                    return <DynamicProduct key={index} brandId={one.brand._id} id={one.shippestProduct._id} img={one.shippestProduct.frontImgURL} fav={false} offer={one.shippestProduct.price !== one.price } oPrice={one.shippestProduct.price} nPrice={one.price} rating={String(one.meanRating)} setNoteMsg={setNoteMsg} uniqueProducts={uniqueProducts} shopRoute={shopRoute}/>
-                  }else{
-                    return <DynamicProduct key={index} brandId={one.product.brand} id={one.product._id} img={one.product.frontImgURL} fav={false} offer={one.product.price !== one.updatedPrice} oPrice={one.product.price} nPrice={one.updatedPrice} rating={String(one.meanRating)} setNoteMsg={setNoteMsg} uniqueProducts={uniqueProducts} shopRoute={shopRoute}/>
-                  }
-                  
-                })
+      {products?.length !== 0 ? (
+        <div dir="rtl" className="flex justify-evenly flex-wrap">
+          {productsDisplayed.map((one, index) => {
+            if (shopRoute) {
+              return (
+                <DynamicProduct
+                  key={index}
+                  brandId={one.brand._id}
+                  id={one.shippestProduct._id}
+                  img={one.shippestProduct.frontImgURL}
+                  fav={false}
+                  offer={one.shippestProduct.price !== one.price}
+                  oPrice={one.shippestProduct.price}
+                  nPrice={one.price}
+                  rating={String(one.meanRating)}
+                  setNoteMsg={setNoteMsg}
+                  uniqueProducts={uniqueProducts}
+                  shopRoute={shopRoute}
+                />
+              );
+            } else {
+              return (
+                <DynamicProduct
+                  key={index}
+                  brandId={one.product.brand}
+                  id={one.product._id}
+                  img={one.product.frontImgURL}
+                  fav={false}
+                  offer={one.product.price !== one.updatedPrice}
+                  oPrice={one.product.price}
+                  nPrice={one.updatedPrice}
+                  rating={String(one.meanRating)}
+                  setNoteMsg={setNoteMsg}
+                  uniqueProducts={uniqueProducts}
+                  shopRoute={shopRoute}
+                />
+              );
             }
+          })}
+        </div>
+      ) : (
+        <div className="h-full flex justify-center items-center">
+          <Lottie animationData={emptyResult} loop={true} />
+        </div>
+      )}
 
-            </div> 
-        ) : (
-            <div className='h-full flex justify-center items-center'>
-              <Lottie animationData={emptyResult} loop={true} />
-            </div>
-        )
-      }
-       
-      
       <ReactPaginateComponent
-      FirstArrow={FirstArrow}
-      LastArrow={LastArrow}
-      currentPage={currentPage}
-      handleChange={handleChange}
-      pageCount={ Math.ceil(products.length / productsPerPage) }/>
-
+        FirstArrow={FirstArrow}
+        LastArrow={LastArrow}
+        currentPage={currentPage}
+        handleChange={handleChange}
+        pageCount={Math.ceil(products?.length / productsPerPage)}
+      />
     </div>
   );
 };
